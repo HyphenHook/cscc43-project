16a8c9191ec92d829f0006b552e440c7f6e47459
JimXYC <yuchuan.xue@mail.utoronto.ca>
2023-08-02 13:04:42 -0400

    Update calculating distance function.

--- project/src/Main.java
+++ project/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         connectionSQL c = new connectionSQL("jdbc:mysql://localhost:3306/project", "root", "204x78y69");
         Availability a = new Availability();
+        search s = new search();
         update up = new update();
         //a.changeAvailability(c, 1, 2023, 9 , 2, 2023, 9, 6, 1);
         //up.createListing(c, 4, "an entire room", 30.0, 20.0, "M1H3K2", "36 Military Trail", "Scarborough", "Canada");
@@ -18,7 +19,8 @@ public class Main {
         //a.updateAvailabilityPrice(c, 7, 2023, 9, 28, 2023, 9,30, 25);
         //a.booking(c, 7, 2023, 9, 1, 2023, 9, 5, 1);
         //up.RemoveListing(c, 7);
-        a.cancelBooking(c, 4, 1);
+        //a.cancelBooking(c, 4, 1);
+        System.out.println(s.getDistance(10, 10, 20, 20));
         //up.RemoveListing(c, 5);
     }
 }
---
+++ project/src/search.java
@@ -0,0 +1,14 @@
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class search {
+    private double getDistance(double lat1, double lon1, double lat2, double lon2){
+        double theta = lon1 - lon2;
+        double distance = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+        distance = Math.toDegrees(Math.acos(distance));
+        double result = distance * 60 * 1.1515 * 1.60934;
+        return result;
+    }
+}
